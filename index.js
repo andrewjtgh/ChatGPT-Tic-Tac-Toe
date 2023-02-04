c9ff9fdda48ead083a0f1bd7f98d7b9a9740cc9b
andrewtan2000 <andrew_tan2000@hotmail.com>
2023-02-04 13:46:00 +0800

    3 files modified

--- index.js
+++ index.js
@@ -2,26 +2,50 @@
 import './style.css';
 
 // Write Javascript code!
-const squares = document.querySelectorAll('.square');
-const turnDisplay = document.querySelector('.turn');
-const resetButton = document.querySelector('.reset-button');
+const squares = document.querySelectorAll(".square");
+const result = document.querySelector(".result");
+const resetButton = document.querySelector(".reset");
+
 let xIsNext = true;
+let winner = null;
 
-for (const square of squares) {
-  square.addEventListener('click', handleClick);
-}
+for (let i = 0; i < squares.length; i++) {
+  squares[i].addEventListener("click", function () {
+    if (winner !== null || squares[i].textContent !== "") {
+      return;
+    }
 
-resetButton.addEventListener('click', resetBoard);
+    if (xIsNext) {
+      squares[i].textContent = "X";
+    } else {
+      squares[i].textContent = "O";
+    }
 
-function handleClick() {
-  if (this.textContent === '') {
-    this.textContent = xIsNext ? 'X' : 'O';
     xIsNext = !xIsNext;
-    turnDisplay.textContent = checkWinner() || `${xIsNext ? 'X' : 'O'}'s turn`;
-  }
+
+    winner = getWinner();
+
+    if (winner === "X") {
+      result.textContent = "X wins!";
+    } else if (winner === "O") {
+      result.textContent = "O wins!";
+    } else if (winner === "tie") {
+      result.textContent = "It's a tie.";
+    }
+  });
 }
 
-function checkWinner() {
+resetButton.addEventListener("click", function () {
+  for (let i = 0; i < squares.length; i++) {
+    squares[i].textContent = "";
+  }
+
+  xIsNext = true;
+  winner = null;
+  result.textContent = "";
+});
+
+function getWinner() {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -30,25 +54,25 @@ function checkWinner() {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
+    [2, 4, 6],
   ];
-  for (const line of lines) {
-    const [a, b, c] = line;
-    if (squares[a].textContent && squares[a].textContent === squares[b].textContent && squares[a].textContent === squares[c].textContent) {
-      return `${squares[a].textContent} wins!`;
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (
+      squares[a].textContent === squares[b].textContent &&
+      squares[a].textContent === squares[c].textContent &&
+      squares[a].textContent !== ""
+    ) {
+      return squares[a].textContent;
     }
   }
-  if ([...squares].every(square => square.textContent)) {
-    return 'Draw';
-  }
-  return null;
-}
 
-function resetBoard() {
-  for (const square of squares) {
-    square.textContent = '';
+  for (let i = 0; i < squares.length; i++) {
+    if (squares[i].textContent === "") {
+      return null;
+    }
   }
-  xIsNext = true;
-  turnDisplay.textContent = `X's turn`;
-}
 
+  return "tie";
+}
